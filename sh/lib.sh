6e12cdc05c63c68de9afd4220568041f788c2934
christian <git@elsee.xyz>
2023-03-18 11:23:34 +0100

    Disable Logger Socket Errors

--- sh/lib.sh
+++ sh/lib.sh
@@ -5,3 +5,5 @@ set -eu
 
 die() { echo "$*" >&2; exit 2; }  # complain to STDERR and exit with error
 needs_arg() { if [ -z "$OPTARG" ]; then die "$( basename $0 ):option requires an argument -- --$OPT"; fi; }
+
+logger() { command logger --socket-errors=off $@ ; }
